d6d7e589f312e670665424069e0dd5acb0cb0fdf
Nathan <usmc_wolf@hotmail.com>
2021-12-22 09:12:46 -0600

    Added react-router-dom so the pages work with no error

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import NavPage from './navLink'
+import PageContent from './page-content'
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Nathan's First Alone React Project</h1>
         
+        <NavPage />
+        <PageContent />
       </div>
     );
   }
---
+++ src/components/colorGame.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import InnerColor from './innerColor'
+
+export default class Game extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Main Color to Match</h1>
+            </div>
+        )
+    }
+}
