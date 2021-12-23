b10d900c731171938d3d0e5353c5c62dac2dc787
Nathan <usmc_wolf@hotmail.com>
2021-12-23 09:49:24 -0600

    found dice rolling npm module trying to get it working

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
-import NavPage from './navLink'
-import PageContent from './page-content'
+import NavPage from './navLink';
+import PageContent from './page-content';
+import D4Dice from './dice/d4'
+
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Nathan's First Alone React Project</h1>
-        
+        <button onClick={D4Dice => console.log(value)}>D4</button>
         <NavPage />
         <PageContent />
       </div>
