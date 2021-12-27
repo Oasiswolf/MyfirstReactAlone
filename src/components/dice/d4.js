2674c1f7f2b49734bf7215a224d475cb357b827c
Nathan <usmc_wolf@hotmail.com>
2021-12-27 13:00:46 -0600

    notes added

--- src/components/dice/d4.js
+++ src/components/dice/d4.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Dice from 'react-dice-roll'
+
 
 export default function() {
     return(
-          
-        <Dice rollingTime={2500} onRoll={value} faces={[1, 2, 3, 4]} defaultValue={4} />
+          <div>
+              
+          </div>
 
     )
 }
